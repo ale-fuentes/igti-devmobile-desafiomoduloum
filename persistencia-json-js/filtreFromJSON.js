b1f8633d02e197a1d7fdcc2931eab6fdf293b36f
ale-fuentes <67701790+ale-fuentes@users.noreply.github.com>
2020-12-01 11:43:04 -0300

    20201201 - Adiciona resolucao em Javascript

---
+++ persistencia-json-js/filtreFromJSON.js
@@ -0,0 +1,184 @@
+var accounts = [{"agencia":10,"conta": 1001 ,"name":"Maria Roberta Fernandes","balance":587},
+{"agencia":10,"conta": 1002 ,"name":"Gustavo Falcao Oliveira","balance":396},
+{"agencia":10,"conta": 1003 ,"name":"Fernando Carlos Silva","balance":500},
+{"agencia":10,"conta": 1004 ,"name":"Aline Batista Bernardes","balance":321},
+{"agencia":10,"conta": 1005 ,"name":"Ivo Jose da Costa","balance":987},
+{"agencia":10,"conta": 1006 ,"name":"Carlos Eduardo Lino","balance":3},
+{"agencia":10,"conta": 1007 ,"name":"Henrique Santos Ferreira","balance":505},
+{"agencia":10,"conta": 1008 ,"name":"Priscila Alves da Cunha","balance":1},
+{"agencia":10,"conta": 1009 ,"name":"Luis Eduardo Nunes","balance":658},
+{"agencia":10,"conta": 1010 ,"name":"Silvana Souto Costa","balance":1023},
+{"agencia":10,"conta": 1011 ,"name":"Julia Maria Alves","balance":985},
+{"agencia":10,"conta": 1012 ,"name":"Bernardo Almeida Alves","balance":842},
+{"agencia":10,"conta": 1013 ,"name":"Lucas Andrade Barbosa","balance":753},
+{"agencia":10,"conta": 1014 ,"name":"Fernanda Barbosa Ferreira","balance":951},
+{"agencia":10,"conta": 1015 ,"name":"Caroline Aparecida da Cunha","balance":126},
+{"agencia":10,"conta": 1016 ,"name":"Ivo Barros","balance":324},
+{"agencia":10,"conta": 1017 ,"name":"Augusto Batista","balance":326},
+{"agencia":10,"conta": 1018 ,"name":"Vania Borges Moreira","balance":634},
+{"agencia":10,"conta": 1019 ,"name":"Valdete Macedo Souto","balance":426},
+{"agencia":10,"conta": 1020 ,"name":"Jose Luis Borges","balance":153},
+{"agencia":10,"conta": 1021 ,"name":"Marcelo Campos","balance":854},
+{"agencia":10,"conta": 1022 ,"name":"Pedro Augusto Cardoso","balance":651},
+{"agencia":10,"conta": 1023 ,"name":"Brena Martinho Carvalho ","balance":358},
+{"agencia":10,"conta": 1024 ,"name":"Pedro Castro","balance":1985},
+{"agencia":10,"conta": 1025 ,"name":"Camila Santini Costa","balance":1874},
+{"agencia":10,"conta": 1026 ,"name":"Alessandra Albergaria Dias","balance":1254},
+{"agencia":10,"conta": 1027 ,"name":"Giovanna Silva Dias","balance":9852},
+{"agencia":10,"conta": 1028 ,"name":"Maria Fernanda Gomes","balance":45},
+{"agencia":10,"conta": 1029 ,"name":"Maria Luiza Nunes","balance":658},
+{"agencia":10,"conta": 1030 ,"name":"Luiza Duarte","balance":452},
+{"agencia":25,"conta": 3001 ,"name":"Roberta Nunes","balance":458},
+{"agencia":25,"conta": 3002 ,"name":"Roberta Miranda","balance":985},
+{"agencia":25,"conta": 3003 ,"name":"Guilherme Zanin Freitas","balance":3265},
+{"agencia":25,"conta": 3004 ,"name":"Guilherme Ferreira Silva","balance":2154},
+{"agencia":25,"conta": 3005 ,"name":"Mario Augusto Fernandes","balance":456},
+{"agencia":25,"conta": 3006 ,"name":"Luis Eduardo Ferreira","balance":981},
+{"agencia":25,"conta": 3007 ,"name":"Claudia Ferreira Garcia","balance":854},
+{"agencia":25,"conta": 3008 ,"name":"Wagner Silva e Silva","balance":568},
+{"agencia":25,"conta": 3009 ,"name":"Daniel Henrique Gomes","balance":548},
+{"agencia":25,"conta": 3010 ,"name":"Lucas Cavalcanti Gonçalves","balance":474},
+{"agencia":25,"conta": 3011 ,"name":"Thiago Oliveira Lima","balance":658},
+{"agencia":25,"conta": 3012 ,"name":"Rogeiro Olveira Lopes","balance":125},
+{"agencia":25,"conta": 3013 ,"name":"Carlos Moniz Machado","balance":365},
+{"agencia":25,"conta": 3014 ,"name":"Rafael Henrique Marques","balance":489},
+{"agencia":25,"conta": 3015 ,"name":"Maira Imai Marques","balance":965},
+{"agencia":25,"conta": 3016 ,"name":"Lucas Portugal Vieira","balance":236},
+{"agencia":25,"conta": 3017 ,"name":"Gustavo Teixeira","balance":452},
+{"agencia":25,"conta": 3018 ,"name":"Cristina Souza","balance":986},
+{"agencia":25,"conta": 3019 ,"name":"Ana Marta Soares","balance":698},
+{"agencia":25,"conta": 3020 ,"name":"Ana Maria Santos","balance":965},
+{"agencia":25,"conta": 3021 ,"name":"Alberto Santana","balance":452},
+{"agencia":25,"conta": 3022 ,"name":"Felipe Ribeiro","balance":365},
+{"agencia":25,"conta": 3023 ,"name":"Henrique Bernardo Rocha","balance":265},
+{"agencia":25,"conta": 3024 ,"name":"Breno Reis","balance":241},
+{"agencia":25,"conta": 3025 ,"name":"Eduardo Olvieira","balance":852},
+{"agencia":25,"conta": 3026 ,"name":"Luis Eduardo Ramos","balance":562},
+{"agencia":25,"conta": 3027 ,"name":"Marcia Pereira","balance":65},
+{"agencia":25,"conta": 3028 ,"name":"Gabriel Gomes Nascimento","balance":325},
+{"agencia":25,"conta": 3029 ,"name":"Victor Moura","balance":965},
+{"agencia":25,"conta": 3030 ,"name":"Ana Maria Silva","balance":5},
+{"agencia":47,"conta": 2201 ,"name":"Jorge Ivan Jimenez","balance":36},
+{"agencia":47,"conta": 2202 ,"name":"Pedro Luis Fragoso","balance":45},
+{"agencia":47,"conta": 2203 ,"name":"Gesiane Leticia Alves","balance":98},
+{"agencia":47,"conta": 2204 ,"name":"Glaucio de Souza","balance":1},
+{"agencia":47,"conta": 2205 ,"name":"Richard Greeson","balance":189},
+{"agencia":47,"conta": 2206 ,"name":"Roberto Lopes Ramos","balance":120},
+{"agencia":47,"conta": 2207 ,"name":"Frederico Saito Rocha Costa","balance":110},
+{"agencia":47,"conta": 2208 ,"name":"Garin da Silva","balance":658},
+{"agencia":47,"conta": 2209 ,"name":"Franciele dos Santos Saraiva","balance":521},
+{"agencia":47,"conta": 2210 ,"name":"Carlos Henrique Soares","balance":136},
+{"agencia":47,"conta": 2211 ,"name":"Wellington Oliveira Filho","balance":215},
+{"agencia":47,"conta": 2212 ,"name":"Gabriela da Rosa Bulla","balance":425},
+{"agencia":47,"conta": 2213 ,"name":"Paola Silva Boff","balance":452},
+{"agencia":47,"conta": 2214 ,"name":"Bruna Caroline Silva de Castro","balance":658},
+{"agencia":47,"conta": 2215 ,"name":"Michele Cigolini Pulsy","balance":452},
+{"agencia":47,"conta": 2216 ,"name":"Eduardo Renan do Nascimento","balance":235},
+{"agencia":47,"conta": 2217 ,"name":"Julie Martin","balance":456},
+{"agencia":47,"conta": 2218 ,"name":"Ricardo Antonio Garcia","balance":987},
+{"agencia":47,"conta": 2219 ,"name":"Belisa Maria de Aguiar","balance":265},
+{"agencia":47,"conta": 2220 ,"name":"Ariana Pereira","balance":125},
+{"agencia":47,"conta": 2221 ,"name":"Guilherme Geraldo Aguiar","balance":3265},
+{"agencia":47,"conta": 2222 ,"name":"Eduardo Renan do Nascimento","balance":111},
+{"agencia":47,"conta": 2223 ,"name":"Gabriela Camila Silva de Lima","balance":214},
+{"agencia":47,"conta": 2224 ,"name":"Ana Carolina de Melo Santos","balance":421},
+{"agencia":47,"conta": 2225 ,"name":"Ana Katia dos Santos Dourado","balance":652},
+{"agencia":47,"conta": 2226 ,"name":"Mauricio Jose da Silva","balance":326},
+{"agencia":47,"conta": 2227 ,"name":"Ana Paula Sena Silva","balance":325},
+{"agencia":47,"conta": 2228 ,"name":"Maria Eduarda de Jesus Santos","balance":253},
+{"agencia":47,"conta": 2229 ,"name":"Angela Cardozo da Silva","balance":523},
+{"agencia":47,"conta": 2230 ,"name":"Ariadne Fernanda de Freitas","balance":587},
+{"agencia":33,"conta": 9101 ,"name":"Denner Saborido de Oliveira","balance":584},
+{"agencia":33,"conta": 9102 ,"name":"Bruno Alvares","balance":654},
+{"agencia":33,"conta": 9103 ,"name":"Marcelo Santos da Silva","balance":256},
+{"agencia":33,"conta": 9104 ,"name":"Utai Oliveira","balance":125},
+{"agencia":33,"conta": 9105 ,"name":"Priscila Milagres Godinho","balance":145},
+{"agencia":33,"conta": 9106 ,"name":"Paulo Dias Macedo Neto","balance":941},
+{"agencia":33,"conta": 9107 ,"name":"Flavio Luis Moreira Pereira","balance":854},
+{"agencia":33,"conta": 9108 ,"name":"Arthur Marcos Calazans Belo","balance":456},
+{"agencia":33,"conta": 9109 ,"name":"Ana Carolina de Melo Santos","balance":874},
+{"agencia":33,"conta": 9110 ,"name":"Ricardo Camacho","balance":325},
+{"agencia":33,"conta": 9111 ,"name":"Thiago Menezes","balance":65},
+{"agencia":33,"conta": 9112 ,"name":"Luis Fernando Toscani","balance":45},
+{"agencia":33,"conta": 9113 ,"name":"Ingrid Martinez Martins","balance":22},
+{"agencia":33,"conta": 9114 ,"name":"Itamir Carvalho Junior","balance":42},
+{"agencia":33,"conta": 9115 ,"name":"Gabriela Correa Alves","balance":125},
+{"agencia":33,"conta": 9116 ,"name":"Vander Lucio Siqueira","balance":965},
+{"agencia":33,"conta": 9117 ,"name":"Taisa Viturino da Silva","balance":745},
+{"agencia":33,"conta": 9118 ,"name":"Norberto Gustavo da Costa","balance":895},
+{"agencia":33,"conta": 9119 ,"name":"Vanessa do Carmo Martins","balance":311},
+{"agencia":33,"conta": 9120 ,"name":"Rosa Maria Sandoval","balance":985},
+{"agencia":33,"conta": 9121 ,"name":"Brunno Leonardo Melo de Oliveira","balance":568},
+{"agencia":33,"conta": 9122 ,"name":"Raissa de Almeida Lima","balance":875},
+{"agencia":33,"conta": 9123 ,"name":"Glaucio de Souza","balance":2354},
+{"agencia":33,"conta": 9124 ,"name":"Julio Cezar Santos","balance":56},
+{"agencia":33,"conta": 9125 ,"name":"Danilo de Souza","balance":98},
+{"agencia":33,"conta": 9126 ,"name":"Diego Luiz Siqueira Teixeira","balance":89},
+{"agencia":33,"conta": 9127 ,"name":"Ariana de Cesare Pereira","balance":126},
+{"agencia":33,"conta": 9128 ,"name":"Eduardo Renan Ferreira","balance":658},
+{"agencia":33,"conta": 9129 ,"name":"Nivaldo Soares do Santos","balance":754},
+{"agencia":33,"conta": 9130 ,"name":"Moises Castillo","balance":857}];
+
+
+let totalSomaBalance = 0;
+accounts.filter(function(item){
+    totalSomaBalance += item.balance;         
+});
+console.log('01] A Soma total de depósitos de todas as agências é: ', totalSomaBalance);
+
+let totalContasComMais100Reais = 0;
+accounts.filter(function(item){
+    totalContasComMais100Reais += (item.balance > 100) ? 1 : 0;         
+});
+console.log("02] O número total de contas com mais de 100 reais de saldo é: ", totalContasComMais100Reais);
+
+let totalContasComMais100ReaisAgencia33 = 0;
+accounts.filter(function(item){
+    totalContasComMais100ReaisAgencia33 += (item.balance > 100 && item.agencia == 33) ? 1 : 0;         
+});
+console.log("03] O número de contas com mais de 100 reais de saldo na agência 33 é: ", totalContasComMais100ReaisAgencia33);
+
+// let agenciaComSaldoMaior = 0;
+// accounts.filter(function(item){
+//     totalContasComMais100ReaisAgencia33 += (item.balance > 100 && item.agencia == 33) ? 1 : 0;         
+// });
+// console.log("03] O número de contas com mais de 100 reais de saldo na agência 33 é: ", totalContasComMais100ReaisAgencia33);
+
+let max = accounts.reduce((p, c) => +p.balance > +c.balance ? p : c).balance;
+let agencia = accounts.filter(d => d.balance == max)
+console.log("04] A agência que tem a conta com maior saldo é a: ", agencia);
+
+console.log("05] Considere o cliente com o maior saldo em cada agência (caso haja mais de um cliente com o maior saldo, escolha apenas um)");
+//const uniqueAgencia = [...new Set(accounts.map(item => item.agencia))]; // [ 'A', 'B']
+const agencias =  accounts.map(item => item.agencia).filter((value, index, self) => self.indexOf(value) === index)
+let totalSaldosMaioresObtidoPorAgencia = 0;
+agencias.forEach(function(agencia){
+    let balances = accounts.filter(d => d.agencia == agencia)
+    let balanceMaiorDaAgencia = balances.reduce((p, c) => +p.balance > +c.balance ? p : c).balance;
+    console.log('agencia: ', agencia, ' valor max ', balanceMaiorDaAgencia);
+    totalSaldosMaioresObtidoPorAgencia += balanceMaiorDaAgencia;
+});
+console.log("O valor total desses saldos é: ", totalSaldosMaioresObtidoPorAgencia);
+
+let contasDaAgencia10 = accounts.filter(d => d.agencia == 10)
+let maiorBalanceDaAgencia10 = contasDaAgencia10.reduce((p, c) => +p.balance > +c.balance ? p : c);
+console.log("06] O nome do(a) cliente com o maior saldo na agência 10 é: ", maiorBalanceDaAgencia10.name);
+
+let contasDaAgencia47 = accounts.filter(d => d.agencia == 47)
+contasDaAgencia47.sort(function (a, b) {
+    return a.balance - b.balance
+});
+console.log("07] O nome do(a) cliente com o menor saldo na agência 47 é: ", contasDaAgencia47[0].name);
+
+console.log("08] Você deve mostrar os nomes dos três clientes com menor saldo em ordem crescente (de saldo menor para o maior), separados por vírgula da agência 47.")
+const ordemPorBalance = contasDaAgencia47.sort(function (a, b) {
+    return a.balance - b.balance
+});
+console.log("Qual seria a sua saída do programa?", ordemPorBalance.slice(0,3).map(a => a.name));
+
+console.log("09] Quantos clientes estão na agência 47? ", contasDaAgencia47.length);
+
+let contasQueContemMariaComoNome = contasDaAgencia47.filter(d => d.name.toLowerCase().includes('maria')).length;
+console.log("10] Quantos clientes que têm 'Maria' no nome estão na agencia 47?", contasQueContemMariaComoNome)
+
+console.log("11] Considerando que o id deve ser único e é sequencial, qual o próximo id possível para conta?", accounts.length + 1);
